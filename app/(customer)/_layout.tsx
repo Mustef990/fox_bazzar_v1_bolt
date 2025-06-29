17b5b65d3289a13126f90ab21d89655d4615143c
Mustef990 <141017276+Mustef990@users.noreply.github.com>
2025-06-29 17:18:18 +0300

    Updated _layout.tsx

--- app/(customer)/_layout.tsx
+++ app/(customer)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Search, ShoppingCart, Heart, User, Wallet, Megaphone, Gift } from 'lucide-react-native';
+import { Chrome as Home, Search, ShoppingCart, Heart, User, Wallet, Megaphone, Gift } from 'lucide-react-native';
 
 export default function CustomerLayout() {
   return (
